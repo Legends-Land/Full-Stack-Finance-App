793433781fd6806fbde48531fd659e2b867e53ac
Legends-Land <caandrews18@gmail.com>
2026-02-06 15:46:37 -0500

    Added user.name to res.jon to login function

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,59 +1,68 @@
 import express from "express";
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import prisma from '../prismaClient.js';
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import prisma from "../prismaClient.js";
 import verifyToken from "../middleware/authmiddleWare.js";
-
 
 //This line creates a new express router
 //Router is a express function that returns an object GET POST PUT DELETE
 //Router can now do everything app can do.
 
-const router = express.Router()
+const router = express.Router();
 
 //Register new account
-router.post('/register', async (req, res) => {
-    const { username, email, password } = req.body 
-    
-    //encrypt password
-    const hashedPassword = bcrypt.hashSync(password, 10)
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
 
-    //save new user and password to the db
-    const user = await prisma.user.create({
-        data: {
-            name: username,
-            password: hashedPassword,
-            email: email
-        }
-    })
+  //encrypt password
+  const hashedPassword = bcrypt.hashSync(password, 10);
 
-    res.status(201).json({ message: "Register route works", user: user });
+  //save new user and password to the db
+  const user = await prisma.user.create({
+    data: {
+      name: username,
+      password: hashedPassword,
+      email: email,
+    },
+  });
+
+  res.status(201).json({ message: "Register route works", user: user });
 });
 
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body
+//User Login//
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-    console.log(req.userId)
+  console.log(req.userId);
 
-    const user = await prisma.user.findUnique({
-        where: {
-            email: email
-        }
-    })
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
 
-    if (!user) {
-        return res.status(404).json({ message: "User not found" })
-    }
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
 
-    const isPasswordValid = bcrypt.compareSync(password, user.password)
+  const isPasswordValid = bcrypt.compareSync(password, user.password);
 
-    if (!isPasswordValid) {
-        return res.status(401).json({ message: "Invalid password" })
-    }
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: "Invalid password" });
+  }
 
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' })
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
 
-    res.json({ message: "Login successful", token: token });
+  res.json({
+    message: "Login successful",
+    token,
+    user: {
+      id: user.id,
+      name: user.name,
+    },
+  });
 });
 
 // const verifyToken = async (req, res, next) => {
@@ -75,14 +84,12 @@ router.post('/login', async (req, res) => {
 
 // }
 
-
-router.get('/home', verifyToken ,async (req, res) => {
-    res.json({ message: "Home route works" });
+router.get("/home", verifyToken, async (req, res) => {
+  res.json({ message: "Home route works" });
 });
 
-router.get('/dashboard', verifyToken, async (req, res) => {
-    res.json({ message: "Dashboard route works" });
+router.get("/dashboard", verifyToken, async (req, res) => {
+  res.json({ message: "Dashboard route works" });
 });
 
-export default router
-
+export default router;
