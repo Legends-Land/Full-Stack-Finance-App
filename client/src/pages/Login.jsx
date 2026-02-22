aa6776ec61c3a4ecbfe8f6bd496f09064e15b9ce
Legends-Land <caandrews18@gmail.com>
2026-02-22 14:39:43 -0500

    Created className to crate transparent background input for login page.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-
-
 const Login = () => {
   const [values, setValues] = useState({
     email: "",
@@ -30,7 +28,7 @@ const Login = () => {
 
       // Navigate to home
       navigate("/dashboard", { replace: true });
-   } catch (err) {
+    } catch (err) {
       if (err.response?.status === 404) {
         setError("We couldn’t find an account with that email.");
       } else if (err.response?.status === 401) {
@@ -45,7 +43,6 @@ const Login = () => {
 
   console.log("Login success, navigating now");
   console.log("token");
-  
 
   // const handleSumbit = async (e) => {
   //     e.preventDefault()
@@ -61,50 +58,75 @@ const Login = () => {
   // }
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="shadow-lg px-8 py-5 border w-72">
-        <h2 className="text-lg font-bold mb-4">Login</h2>
-        <form onSubmit={handleSumbit}>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-700">
-              Email
-            </label>
-            <input
-              type="email"
-              placeholder="Enter Email"
-              className="w-full px-3 py-2 border"
-              name="email"
-              required
-              onChange={handleChanges}
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-gray-700">
-              Password
-            </label>
-            <input
-              type="password"
-              placeholder="Enter Password"
-              className="w-full px-3 py-2 border"
-              name="password"
-              required
-              onChange={handleChanges}
-            />
-          </div>
-          <button className="w-full bg-green-600 text-white py-2 ">
-            Submit
-          </button>
-        </form>
-        <div className="text-center">
-          <span>Don't Have Account?</span>
-          <Link to="/register" className="text-blue-500">
-            Signup
-          </Link>
-           {error && (
-            <div className="text-red-500 mt-3 text-sm text-center">
-              {error}
+    <div className="container-login">
+      <div className="login-logo"></div>
+      <div className="login-page">
+        <div className="flex justify-center items-center h-screen">
+          <div className="shadow-lg px-8 py-5 border w-72">
+            <h2 className="text-lg font-bold mb-4">Welcome to Coinage</h2>
+            <form onSubmit={handleSumbit}>
+              <div className="mb-4">
+                <label htmlFor="email" className="block text-gray-700">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  placeholder="Enter Email"
+                  className="login-input"
+                  name="email"
+                  required
+                  onChange={handleChanges}
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="password" className="block text-gray-700">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  className="email-input"
+                  name="password"
+                  required
+                  onChange={handleChanges}
+                />
+              </div>
+              <button className="w-full bg-green-600 text-white py-2 ">
+                Submit
+              </button>
+              {/* <button className="w-full bg-green-600 text-white py-2 ">
+                Sign Up
+              </button> */}
+
+              {/* <div>
+                <button className="w-full bg-green-600 text-white py-2 ">
+                  Sign Up
+                </button>
+              </div> */}
+            </form>
+
+            <div className="text-center">
+              <span>Don't Have Account? </span>
+
+              <Link to="/register" className="text-blue-500">
+                Signup
+              </Link>
+
+              {error && (
+                <div className="text-red-500 mt-3 text-sm text-center">
+                  {error}
+                </div>
+              )}
             </div>
-          )}
+          </div>
+
+          <div>
+            <img
+              src="images/Coinage-logo.png"
+              className="login-logo2"
+              alt="Company-logo"
+            />
+          </div>
         </div>
       </div>
     </div>
